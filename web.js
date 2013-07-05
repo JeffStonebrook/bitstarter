8b9328e678d95eb258e5c099d3680efa40338def
Ubuntu <ubuntu@domU-12-31-39-09-C8-23.compute-1.internal>
2013-07-05 22:39:56 +0000

    Removing console.log - its working now

--- web.js
+++ web.js
@@ -4,11 +4,8 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  console.log("About to read index.html from file");
   var buffer = fs.readFileSync("index.html");
-  console.log("Read in file");
   response.send(buffer.toString());
-  console.log("Printed to response");
 });
 
 var port = process.env.PORT || 5000;
